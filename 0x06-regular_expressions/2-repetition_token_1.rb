1ab736f5e5e87e79a0ef0b0851d6e8cd192bbda3
tabbykatz <tomelay@gmail.com>
2020-07-06 19:01:54 +0000

    initial commit

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/127.0.0.[0-9]/).join
+sylvain@ubuntu$
+sylvain@ubuntu$ ./example.rb 127.0.0.2
+127.0.0.2
+sylvain@ubuntu$ ./example.rb 127.0.0.1
+127.0.0.1
+sylvain@ubuntu$ ./example.rb 127.0.0.a
