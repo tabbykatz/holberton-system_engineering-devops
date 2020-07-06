f433c75fc0ec89b221f6808d79b889705cd76ef9
tabbykatz <tomelay@gmail.com>
2020-07-06 22:54:29 +0000

    add all tasks

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,8 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/127.0.0.[0-9]/).join
-sylvain@ubuntu$
-sylvain@ubuntu$ ./example.rb 127.0.0.2
-127.0.0.2
-sylvain@ubuntu$ ./example.rb 127.0.0.1
-127.0.0.1
-sylvain@ubuntu$ ./example.rb 127.0.0.a
+puts ARGV[0].scan(/hbt{2,5}n/).join
