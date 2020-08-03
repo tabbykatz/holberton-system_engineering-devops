26c44119bb88b06a9633b0ffe008130ee25c348d
tabbykatz <tomelay@gmail.com>
2020-08-03 22:31:33 +0000

    0x0A, first pass

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file
+file { '/tmp/holberton':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,3 @@
+# kill a process called killmenow
+exec { '/usr/bin/env pkill -9 killmenow':
+}
